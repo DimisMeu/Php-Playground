6883c4de21ddf2def98616d28e26cdc2cc8c12a4
DimisM <9743028+DimisMeu@users.noreply.github.com>
2023-01-31 09:02:01 +0200

    Update WOD_Generator.php

--- WOD_Generator.php
+++ WOD_Generator.php
@@ -1,5 +1,5 @@
 <?php
-  $movements = array("Burpees", "Squats", "Deadlifts", "Lunges", "Push-ups", "Sit-ups", "Box Jumps", "Pull-ups", "Handstand Push-ups", "Double Unders");
+  $movements = array("Burpees", "Squats", "Deadlifts", "Lunges", "Push-ups", "Sit-ups", "Box Jumps", "Pull-ups", "Handstand Push-ups", "Double Unders", "Snatch", "Clean", "Clean and Jerk", "Front Squat", "Overhead Squat", "Overhead Press", "Bench Press", "Push Press", "Back Squat", "Squat Jerk", "Split Jerk", "Barbell Row", "Thruster‘", "Ring Muscle Up", "Bar Muscle Up", "Ring Handstand Push Up", "Handstand Walk", "Wall Balls", "Box Jumps", "Rope Climbing", "Kettlebell Swings");
   $rounds = rand(3, 5);
   $reps = rand(10, 20);
 
